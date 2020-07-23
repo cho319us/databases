81971d44ac602b314d2b3e12d7636f1cf8a5e70b
Carrie Guan <carrieguan17@gmail.com>
2020-07-23 15:02:31 -0700

    get and post for message complete

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -2,27 +2,28 @@ var models = require('../models');
 
 module.exports = {
   get: function (req, res) {
-    console.log('testing');
     models.messages.getAll((err, results) => {
       if (err) {
-        res.writeHead('404');
-        console.log('here is controller messages.js with error');
+        res.writeHead(404);
         res.end(err);
       } else {
-        console.log('here is controller success messages.js');
         res.writeHead(201);
         res.end(JSON.stringify(results));
       }
     });
   }, // a function which handles a get request for all messages
   post: function (req, res) {
-    console.log('this is request', req);
-    console.log('what type?', typeof (req));
-    // var queryArg = [,,];
-    // models.create(queryArg, (err, results) => {
-
-    // });
-    res.end();
+    var queryArg = [req.body.text, req.body.username, req.body.chatroom];
+    console.log(queryArg);
+    models.messages.create(queryArg, (err, results) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(err);
+      } else {
+        res.writeHead(201);
+        res.end();
+      }
+    });
   } // a function which handles posting a message to the database
 };
 
--- server/models/messages.js
+++ server/models/messages.js
@@ -1,16 +1,14 @@
-var dbConnection = require('../db');
+var db = require('../db');
 
 module.exports = {
   getAll: function (callback) {
     // define queryString
     var queryString = 'select users.username, messages.roomname, messages.messagetext, messages.id from users, messages where users.id = messages.userid';
     // use db.query to read the db to obtain the messeages
-    dbConnection.query(queryString, (err, results) => {
+    db.query(queryString, (err, results) => {
       if (err) {
-        console.log('here is model meesage err');
         callback(err);
       } else {
-        console.log('here is model message success');
         callback(null, results);
       }
     });
@@ -18,8 +16,6 @@ module.exports = {
   create: function (queryArg, callback) {
     // define queryString
     var queryString = 'insert into messages(messagetext, userid, roomname) values (?, (select id from users where username = ?), ?)';
-    //" + '.$messeagetext' + "," + '.$userid' + "," + '.$roomname' + ")";
-    // ,(select id from users where username = ?), ?)';
     // use db.query to insert a message into the db
     db.query(queryString, queryArg, (err, data) => {
       if (err) {
